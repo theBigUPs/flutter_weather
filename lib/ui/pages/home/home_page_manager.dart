206b75d989c64a25320fb3c37fe8a60564393269
theBigUPs <uygrprvn@gmail.com>
2023-12-18 09:44:37 +0300

    added degree symbol to text

--- lib/ui/pages/home/home_page_manager.dart
+++ lib/ui/pages/home/home_page_manager.dart
@@ -3,16 +3,14 @@ import 'package:flutter_weather/services/local_storage.dart';
 import 'package:flutter_weather/services/service_adapter.dart';
 import 'package:flutter_weather/services/web_api.dart';
 
-class HomePageManager{
+class HomePageManager {
   HomePageManager({WebApi? webApi, LocalStorage? storage}) {
     _webApi = webApi ?? getIt<WebApi>();
     _storage = storage ?? getIt<LocalStorage>();
   }
 
-
   late WebApi _webApi;
   late LocalStorage _storage;
-
 
   final loadingNotifier = ValueNotifier<LoadingStatus>(const Loading());
   final temperatureNotifier = ValueNotifier<String>('');
@@ -20,35 +18,35 @@ class HomePageManager{
 
   late double _temperature;
 
-  Future<void> loadWeather() async
-  {
+  Future<void> loadWeather() async {
     loadingNotifier.value = const Loading();
     final isCelsius = _storage.isCelsius;
-    buttonNotifier.value = isCelsius ? '°C' : '°F'; 
-    try
-    {
-      final weather= await _webApi.getWeather(latitude: 41.0138, longitude: 28.9497);
-      _temperature=weather.temp;
+    buttonNotifier.value = isCelsius ? '°C' : '°F';
+    try {
+      final weather =
+          await _webApi.getWeather(latitude: 41.0138, longitude: 28.9497);
+      _temperature = weather.temp;
 
-      final temperature= isCelsius ? _temperature : _convertToFahrenheit(_temperature);
-      temperatureNotifier.value = '$temperature';
-      loadingNotifier.value = LoadingSuccess(weather: weather.desc,);
-
-    }
-    catch(e)
-    {
+      final temperature =
+          isCelsius ? _temperature : _convertToFahrenheit(_temperature);
+      temperatureNotifier.value = '$temperature°';
+      loadingNotifier.value = LoadingSuccess(
+        weather: weather.desc,
+      );
+    } catch (e) {
       loadingNotifier.value = const LoadingError("Couldn't load the weather");
     }
   }
 
   double _convertToFahrenheit(double temperature) {
-    return (temperature * 9/5 + 32);
+    return (temperature * 9 / 5 + 32);
   }
 
   void convertTemperature() {
     final isCelsius = !_storage.isCelsius;
     _storage.saveIsCelsius(isCelsius);
-    final temperature = (isCelsius) ? _temperature : _convertToFahrenheit(_temperature);
+    final temperature =
+        (isCelsius) ? _temperature : _convertToFahrenheit(_temperature);
     temperatureNotifier.value = '$temperature°';
     buttonNotifier.value = (isCelsius) ? '°C' : '°F';
   }
