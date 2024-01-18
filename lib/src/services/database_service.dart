ec35940a5f15912a1f34ce2e4a03e60796e8cd11
theBigUPs <114905556+theBigUPs@users.noreply.github.com>
2024-01-18 15:53:54 +0300

    saveusersettings updated but not integrated

--- lib/src/services/database_service.dart
+++ lib/src/services/database_service.dart
@@ -1,36 +1,32 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 abstract interface class DatabaseService {
-  Future<void> saveUserSettings(List<String> cities,String userid,String startingCity);
+  Future<void> saveUserSettings(List<String> cities, String userid,
+      String startingCity, bool isCelsius, bool isLocationOn);
   Future<void> getUserSettings(String userid);
   Future<void> deleteUserSettings(String userid);
-
 }
 
-class FirebaseDatabaseService implements DatabaseService 
-{
+class FirebaseDatabaseService implements DatabaseService {
   final FirebaseFirestore _store = FirebaseFirestore.instance;
   @override
-  Future<void> deleteUserSettings(String userid) async{
+  Future<void> deleteUserSettings(String userid) async {
     await _store.collection("user_settings").doc(userid).delete();
   }
 
-  
-
   @override
-  Future<void> getUserSettings(String userid) async{
+  Future<void> getUserSettings(String userid) async {
     await _store.collection("user_settings").doc(userid).get();
   }
 
-
   @override
-  Future<void> saveUserSettings(List<String> cities,String userid,String startingCity) async{
-    await _store.collection("user_settings").doc(userid).set(
-      {
-        "citylist":cities,
-        "startingcity":startingCity
-
-      });
+  Future<void> saveUserSettings(List<String> cities, String userid,
+      String startingCity, bool isCelsius, bool isLocationOn) async {
+    await _store.collection("user_settings").doc(userid).set({
+      "citylist": cities,
+      "startingcity": startingCity,
+      "iscelsius": isCelsius,
+      "islocationon": isLocationOn
+    });
   }
-
 }
